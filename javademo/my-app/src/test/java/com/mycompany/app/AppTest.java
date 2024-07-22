0934f25bd2a8f5fec75aec446e45c8d55b98e068
Rivo03 <164886704+Rivo03@users.noreply.github.com>
2024-07-22 14:01:34 +0000

    test(javadev): test

--- javademo/my-app/src/test/java/com/mycompany/app/AppTest.java
+++ javademo/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,9 +1,14 @@
 package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
+
+import java.beans.Transient;
+
 import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
+
+import com.mycompany.app.App;
 
 /**
  * Unit test for simple App.
@@ -22,5 +27,13 @@ public class AppTest
     public void shouldAnswerWithFalse()
     {
         assertFalse( false );
+    }
+    @Test
+    public void test(){
+        App.main(null);
+    }
+    @Test
+    public void test_instanza(){
+        new App();
     }
 }
